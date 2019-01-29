51651c2d9c8fa4222422571bc2bc410d3a81e9ed
Adelice <nancytoniw@gmail.com>
2019-01-29 17:24:22 +0200

    save changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,7 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes=[
-    new Quote(1, '"Be yourself; everyone else is already taken"','Oscar wilde',0,0),
+    new Quote(1, '"Be yourself; everyone else is already taken"','Oscar Wilderich',0,0),
     new Quote(2,'"Be who you are and say what you feel because those who mind dont matter and those who matter dont mind."','Bernard M. Baruch',0,0 ),
     new Quote(3,'“A room without books is like a body without a soul."','Marcus Tullius Cicero',0,0)
   ]
